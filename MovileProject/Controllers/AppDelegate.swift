84c814e00ef62178fe3397eeaa81e20644c57472
iOS <ios@MAC-128.nabs.com.br>
2015-07-15 14:47:43 -0300

    Initial Commit

---
+++ MovileProject/Controllers/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  MovileProject
+//
+//  Created by iOS on 7/15/15
+//  Copyright (c) 2015 . All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
